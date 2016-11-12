a5ff8ece9e69abd729aefa43caa1cdd9bc2a86e7
Tecnología Digital del Bajío <ventas@tecdigitaldelbajio.com>
2016-11-11 20:27:40 -0600

    Add files via upload

---
+++ PIC24FJ64GA002/PIC24FJ64GA002_Blink.c
@@ -0,0 +1,15 @@
+#include <PIC24FJ64GA002_Blink.h>
+
+
+void main()
+{
+   //Example blinking LED program
+   while(true)
+   { 
+      output_low(LED);
+      delay_ms(DELAY);
+      output_high(LED);
+      delay_ms(DELAY);
+   }
+
+}
